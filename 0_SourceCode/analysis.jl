46285d9582381391988b91c7dd909c77a6bd8f5c
pclusella <pau.clusella@upf.edu>
2023-01-06 13:17:35 +0100

    Julia script and subdirectories names changed

---
+++ 0_SourceCode/analysis.jl
@@ -0,0 +1,36 @@
+using DelimitedFiles
+
+t0=200;
+tf=300;
+id=350;
+
+function read_channel(id,t0,tf) # Equivalent to the read_binary.c code
+    id=Int(id);
+    fin=open("../1_Raw/pre.bin","r");
+    n = 384; 
+    m = 2250000;
+    dt=1.0/2500.0;
+
+    m0=Int(ceil(t0/dt));
+    mf=Int(floor(tf/dt));
+
+    data=zeros(n);
+
+    for i in 1:(m0-1)
+        read!(fin,data);
+    end
+    dm=mf-m0+1;
+    channel=zeros(dm);
+    for i in 1:dm
+        read!(fin,data);
+        channel[i]=data[id];
+    end
+    close(fin);
+
+    t=collect(dt*(m0:mf));
+    writedlm("../temp.dat",[t channel]," ",);
+    return t, channel;
+end
+
+t,dat=read_channel(id,t0,tf);
+
