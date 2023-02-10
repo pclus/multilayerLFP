8479415525aa113c6b51cb98f7d5d58882103137
pclus <pau.clusella@upf.edu>
2023-02-10 11:54:50 +0100

    Further developing kCSD for the whole time series

--- 0_SourceCode/kernelCSD.py
+++ 0_SourceCode/kernelCSD.py
@@ -1,35 +1,11 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from kcsd import KCSD2D
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-# -- 
-# Define electrode locations (centers of the 12x12 squares)
-x_pos = np.array([29, 61, 13, 45 ])
-ele_x = np.tile(x_pos,96)
-ele_y = [i for i in range(6,3840, 20) for j in range(2)]
-ele_pos = np.column_stack((ele_x, ele_y))
-# --
-
-# --
-# Save and plot locations:
-# np.savetxt('electrode_locations.dat',ele_pos)
-# p1=plt.scatter(ele_pos[:, 0], ele_pos[:, 1], 10, c='k')
-# plt.show()
-# --
-
-# -- read a single data point [THIS SHOUT BE REMOVED SOON]
-def read_data_point(j):
-   n= 384
-   m = 2250000; 
-   data=np.zeros(n)
-   with open("/home/pclusella/Documents/Data/UPO-tACs/1_Raw/pre.bin", "rb") as fin:
-       data[0]=np.fromfile(fin, dtype=np.double, count=1, sep='',offset=8*(j-1))
-       for i in range(1,n):
-           data[i]=np.fromfile(fin, dtype=np.double, count=1, sep='', offset=8*(m-1))
-   return data;
-# --
+# ---- Functions ----
 
 # --
 # Read the bindary file "pre.bin"
@@ -48,15 +24,26 @@ def read_data_chunk(t0,tf):
     return data;
 # --
 
-pots =read_data_chunk(0.0004,100.0)
+# --
+# modifyied from the KCSD tutorial.
+def do_kcsd(ele_pos, pots):
+    h = 10.     # thickness
+    sigma = 1.0 # S/m
+    length = pots.shape[1] # size of time dimensions
+    pots = pots.reshape((len(ele_pos), length)) 
+    k = KCSD2D(ele_pos, pots, h=h, sigma=sigma,                                                                                                                                                       
+               xmin=-30.0, xmax=100.0,
+               ymin=-60.0, ymax=3900.0,
+               n_src_init=1000, src_type='gauss', R_init=40,lambd=1e-5, gdx=5.0,gdy=5.0) # gdx and gdy are the spatial resolutions
+    return k
+# --
 
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-
-def make_plot(xx, yy, zz, title='True CSD', cmap=cm.bwr):
+# --
+# modifyied from the KCSD tutorial
+# can be used with: ax=make_plot(k.estm_x, k.estm_y, est_csd[:, :, 0], cmap=cm.bwr) 
+def make_plot(xx, yy, zz, title='CSD', cmap=cm.bwr):
     fig = plt.figure(figsize=(7, 7))
     ax = plt.subplot(111)
-    # ax.set_aspect('equal')
     t_max = np.max(np.abs(zz))
     levels = np.linspace(-1 * t_max, t_max, 32)
     im = ax.contourf(xx, yy, zz, levels=levels, cmap=cmap)
@@ -66,31 +53,30 @@ def make_plot(xx, yy, zz, title='True CSD', cmap=cm.bwr):
     ticks = np.linspace(-1 * t_max, t_max, 3, endpoint=True)
     plt.colorbar(im, orientation='horizontal', format='%.2f', ticks=ticks)
     return ax
+# -- 
 
+# ---- Workflow ----
 
-def do_kcsd(ele_pos, pots):
-    h = 10.  # thickness
-    sigma = 1.0 # S/m
-    # pots = pots.reshape((len(ele_pos), 1)) # one time point 
-    # k = KCSD2D(ele_pos, pots, h=h, sigma=sigma,                                                                                                                                                       
-    #            xmin=-30.0, xmax=100.0,
-    #            ymin=-60.0, ymax=3900.0,
-    #            n_src_init=1000, src_type='gauss', R_init=40,lambd=1e-5, gdx=5.0,gdy=5.0) # gdx and gdy are the spatial resolutions
+# -- 
+# Define electrode locations (centers of the 12x12 squares)
+x_pos = np.array([29, 61, 13, 45 ])
+ele_x = np.tile(x_pos,96)
+ele_y = [i for i in range(6,3840, 20) for j in range(2)]
+ele_pos = np.column_stack((ele_x, ele_y))
+# --
 
-    length = pots.shape[1]
-    pots = pots.reshape((len(ele_pos), length)) # 2500 time points
-    k = KCSD2D(ele_pos, pots, h=h, sigma=sigma,                                                                                                                                                       
-               xmin=-30.0, xmax=100.0,
-               ymin=-60.0, ymax=3900.0,
-               n_src_init=1000, src_type='gauss', R_init=40,lambd=1e-5, gdx=5.0,gdy=5.0) # gdx and gdy are the spatial resolutions
-    return k
+# --
+# Save and plot locations:
+# np.savetxt('electrode_locations.dat',ele_pos)
+# p1=plt.scatter(ele_pos[:, 0], ele_pos[:, 1], 10, c='k')
+# plt.show()
+# --
 
-
+# -- 
+# Read 100 data points, compute, and plot the kCSD
+pots =read_data_chunk(0.0004,100.0)
 k = do_kcsd(ele_pos, pots)
 est_csd = k.values('CSD')
-
-# est_csd = k.values('POT')
-# np.savetxt('temp.dat',est_csd[:,:,0])
 
 # --
 f1=plt.figure(1)
@@ -107,9 +93,16 @@ plt.show()
 # plt.show()
 # --
 
-ax=make_plot(k.estm_x, k.estm_y, est_csd[:, :, 0], 
-          title='Estimated CSD without CV', cmap=cm.bwr) # First time point
-plt.show()
+# --
+""""
+Since we cannot load the whole time series we'll have to take chuncks of 100 miliseconds,
+store them in different binaries, and then reorder the chunkcs in a unique big fatty binary file
+"""" 
+
+
+
+# est_csd = k.values('POT')
+# np.savetxt('temp.dat',est_csd[:,:,0])
 
 
 # # f2=plt.figure(2)
