ed973cd3aab658fec196855587817da5b79bbddf
pclus <pau.clusella@upf.edu>
2023-05-03 14:56:39 +0200

    data fixed in threading

--- 0_codes/thread.sh
+++ 0_codes/thread.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {20,};
+for i in {8,9,10,11,12,13,15,16,18,20,19};
 do
 	julia main.jl $i
+	DEST="~/Documents/Athena/UPO_data_processed/Suj${i}"
+	mkdir -p ${DEST}
+	mv ../1_data/*bin ${DEST}/
+	mv ../1_data/mov*dat ${DEST}/ 
 done
 
-#DEST="/media/pclusella/Seagate\ Hub/Athena/UPO_data_processed/Suj${i}"
-#mkdir -p ${DEST}
-#mv ../*bin ${DEST}/
-#mv ../mov*dat ${DEST}/ 
